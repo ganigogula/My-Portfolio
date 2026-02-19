f6ab403176da72874d9b23118b7575a1518f9767
Ganesh Gogula <ganeshgogula6@gmail.com>
2026-02-19 13:58:34 +0530

    first commit

---
+++ Myportfolio/script.js
@@ -0,0 +1,7 @@
+// Toggle Menu for Mobile
+const hamburger = document.getElementById('hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
