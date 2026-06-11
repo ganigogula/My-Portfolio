700c4844d493f46a765c265bcbef8345d4dde368
Ganesh Gogula <ganeshgogula6@gmail.com>
2026-06-11 12:44:50 +0530

    Remove old ecommerce and duplicate portfolio folders

--- Myportfolio/script.js
+++
@@ -1,7 +0,0 @@
-// Toggle Menu for Mobile
-const hamburger = document.getElementById('hamburger');
-const menu = document.querySelector('.menu');
-
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
